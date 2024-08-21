461290d44706c5f916768af18174c99e30cb6ac3
Garima <sharma12garima@gmail.com>
2024-08-21 12:05:42 +0200

    The HDI of Labour Force Participation is cleaned

---
+++ venv/Lib/site-packages/pyogrio/__init__.py
@@ -0,0 +1,75 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_6_0():
+    import ctypes
+    import os
+    import platform
+    import sys
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'pyogrio.libs'))
+    is_conda_cpython = platform.python_implementation() == 'CPython' and (hasattr(ctypes.pythonapi, 'Anaconda_GetVersion') or 'packaged by conda-forge' in sys.version)
+    if sys.version_info[:2] >= (3, 8) and not is_conda_cpython or sys.version_info[:2] >= (3, 10):
+        if os.path.isdir(libs_dir):
+            os.add_dll_directory(libs_dir)
+    else:
+        load_order_filepath = os.path.join(libs_dir, '.load-order-pyogrio-0.9.0')
+        if os.path.isfile(load_order_filepath):
+            with open(os.path.join(libs_dir, '.load-order-pyogrio-0.9.0')) as file:
+                load_order = file.read().split()
+            for lib in load_order:
+                lib_path = os.path.join(os.path.join(libs_dir, lib))
+                kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
+                if os.path.isfile(lib_path) and not kernel32.LoadLibraryExW(ctypes.c_wchar_p(lib_path), None, 0x00000008):
+                    raise OSError('Error loading {}; {}'.format(lib, ctypes.FormatError(ctypes.get_last_error())))
+
+
+_delvewheel_patch_1_6_0()
+del _delvewheel_patch_1_6_0
+# end delvewheel patch
+
+try:
+    # we try importing shapely, to ensure it is imported (and it can load its
+    # own GEOS copy) before we load GDAL and its linked GEOS
+    import shapely  # noqa
+    import shapely.geos  # noqa
+except Exception:
+    pass
+
+from pyogrio.core import (
+    list_drivers,
+    detect_write_driver,
+    list_layers,
+    read_bounds,
+    read_info,
+    set_gdal_config_options,
+    get_gdal_config_option,
+    get_gdal_data_path,
+    __gdal_version__,
+    __gdal_version_string__,
+    __gdal_geos_version__,
+)
+from pyogrio.raw import read_arrow, open_arrow, write_arrow
+from pyogrio.geopandas import read_dataframe, write_dataframe
+from pyogrio._version import get_versions
+
+
+__version__ = get_versions()["version"]
+del get_versions
+
+__all__ = [
+    "list_drivers",
+    "detect_write_driver",
+    "list_layers",
+    "read_bounds",
+    "read_info",
+    "set_gdal_config_options",
+    "get_gdal_config_option",
+    "get_gdal_data_path",
+    "read_arrow",
+    "open_arrow",
+    "write_arrow",
+    "read_dataframe",
+    "write_dataframe",
+    "__gdal_version__",
+    "__gdal_version_string__",
+    "__gdal_geos_version__",
+    "__version__",
+]
